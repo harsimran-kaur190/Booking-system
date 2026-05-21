97c458673812a2d0d277276fbbf713860525ddda
Harsimran Kaur <harsimran291972@gmail.com>
2026-05-21 22:07:33 +0530

    deploy

---
+++ js/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  API_KEY: "c7606b85ed7439c983d4d2227644dbfa",
+  BASE_URL: "https://api.themoviedb.org/3",
+  IMAGE_URL: "https://image.tmdb.org/t/p/w500",
+  BACKDROP_URL: "https://image.tmdb.org/t/p/original",
+  LANGUAGE: "en-US"
+};
